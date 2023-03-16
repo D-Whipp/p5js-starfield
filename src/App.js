1d953006216ff488add91d8146b0cfdb9ad88dd2
Mr Whipp <dwhipp88@gmail.com>
2023-03-15 23:29:27 -0600

    created basic setup, added p5js to react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Sketch from 'react-p5';
+
+const App = () => {
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(500, 400).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.background(255, 130, 20);
+    p5.ellipse(100, 100, 100);
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+};
+
+export default App;
